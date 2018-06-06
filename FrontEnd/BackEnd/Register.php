e5fc9196eb5f6d410ec4a9b7cd2a63d487986f23
BogdanFi <bogdan_star1@yahoo.com>
2018-06-06 11:57:51 +0300

    v2

--- FrontEnd/BackEnd/Register.php
+++ FrontEnd/BackEnd/Register.php
@@ -1,22 +1,51 @@
 <?php
 
 if(isset($_POST['submit']))
-{
-include 'db.php';
+	{
+	include 'db.php';
 
-$first= $_POST['fname'];
-$last= $_POST['lname'];
-$email=$_POST['email'];
-$uid= $_POST['username'];
-$pwd= $_POST['pass'];
-$privilegii='0';
+	$first=mysqli_real_escape_string($conn,$_POST['fname']);
+	$last=mysqli_real_escape_string($conn,$_POST['lname']);
+	$email=mysqli_real_escape_string($conn,$_POST['email']);
+	$uid=mysqli_real_escape_string($conn,$_POST['username']);
+	$pwd=mysqli_real_escape_string($conn,$_POST['pass']);
+	$privilegii='0';
 
-$sql= "INSERT INTO site_users (first_name,last_name,email,username,password,privilegii) VALUES ('$first','$last','$email','$uid','$pwd','$privilegii')";
-$result=mysqli_query($conn,$sql);
-
-header("Location:../Auctiox/FrontEnd/register.php");
-}
+	if(!preg_match("/^[a-zA-Z]*$/",$first)||!preg_match("/^[a-zA-Z]*$/",$last) )
+		{
+		header("Location:..//Auctiox/FrontEnd/register.php?=register=invalid");
+		exit();
+		}
+		else
+			{
+			if(!filter_var($email,FILTER_VALIDATE_EMAIL))
+				{
+				header("Location:..//Auctiox/FrontEnd/register.php?=register=email");
+				exit();
+				}
+			else
+				{
+				$verificare="SELECT * FROM site_users WHERE user_id='$uid'";
+				$apel=mysqli_query($conn,$verificare);
+				$apelCheck=mysqli_num_rows($result);
+				
+				if($apelCheck >0)
+					{
+					header("Location:..//Auctiox/FrontEnd/register.php?=register=userulExista");
+					exit();	
+					}
+					else
+						{
+						$hashedPwd=password_hash($pwd, password_default);
+						$sql= "INSERT INTO site_users (first_name,last_name,email,username,password,privilegii) VALUES ('$first','$last','$email','$uid','$pwd','$privilegii')";
+						mysqli_query($conn,$sql);
+						header("Location:../Auctiox/FrontEnd/register.php?=register=Succes");
+						exit();
+				}	
+			}
+		}
+	}
 else
-{
-	header("Location:../register.php");
-}
+	{
+		header("Location:../register.php");
+	}
