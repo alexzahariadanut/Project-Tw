4c514d805f15df239007073cf901aacd4e12d3e9
alexzahariadanut <alexzahariadanut@users.noreply.github.com>
2018-06-10 10:51:03 +0100

    Add files via upload

---
+++ FrontEnd/search.php
@@ -0,0 +1,45 @@
+<?php
+	include 'header.php';
+?>
+
+<h1>Search page</h1>
+<div class="article-container">
+<?php	
+
+	if(isset($_POST['submit-search']))
+	{
+		$search = mysqli_real_escape_string($conn, $_POST['search']);
+		$sql = "Select * from article where a_title LIKE '%$search%' or a_text like '%$search%' 
+		or a_author like '%$search%' or a_date like '%$search%'";
+		$result = mysqli_query($conn,$sql);
+		$queryResult = mysqli_num_rows($result);
+		
+		echo "There are".$queryResult. "results!";
+		
+		if($queryResult > 0 )
+		{
+			while($row = mysqli_fetch_assoc($result))
+				
+			{
+				echo "<a href='article.php?title=".$row['a_title']."&date=".$row['a_date']."'><div class ='article-box'>
+						<h3>".$row['a_title']."</h3>
+						<p>".$row['a_text']."</p>
+						<p>".$row['a_date']."</p>
+						<p>".$row['a_author']."</p>
+					</div></a>";
+				
+			}
+			
+		}
+		else
+		{
+			echo "There are no results matching your search!";
+		}
+			
+			
+		
+		
+		
+	}
+?>
+</div>
