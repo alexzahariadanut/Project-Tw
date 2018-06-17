2b1e317a99c20a70738c0aba6b92d48b59db854b
alexzahariadanut <alexzahariadanut@users.noreply.github.com>
2018-06-17 20:40:23 +0300

    Update produsAdmin.php

--- produsAdmin.php
+++ produsAdmin.php
@@ -36,15 +36,17 @@
 			</div>
 		</header>
 		
-		<section id="newsletter">
+		
+		
+			<section id="newsletter">
 			<div class="container">
-				<form action="searchAdmin.php">
-					<input type="search" name="q" placeholder="Search" required>
-					<button type="Submit" class="button_1">Search</button>
-				</form>
-			
-			</div>
-		</section>
+			<form action="searchadmin.php" method="POST">
+				<input  type="text" name="search" placeholder="Search">
+				<button type="submit" name="submit-search" class="button_1">Search</button>
+			</form>
+
+				
+			</section>
 		
 		<section id="main">
 			<div class="container">		
