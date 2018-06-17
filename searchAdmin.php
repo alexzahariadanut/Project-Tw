366bdc18b5b14ff26e90116ea3ce817ccfd04031
alexzahariadanut <alexzahariadanut@users.noreply.github.com>
2018-06-17 20:45:22 +0300

    Update searchAdmin.php

--- searchAdmin.php
+++ searchAdmin.php
@@ -52,7 +52,7 @@
 		
 		<section id="newsletter">
 			<div class="container">
-			<form action="searchafterlogin.php" method="POST">
+			<form action="searchadmin.php" method="POST">
 				<input  type="text" name="search" placeholder="Search" required>
 				<button type="submit" name="submit-search" class="button_1">Search</button>
 			</form>
