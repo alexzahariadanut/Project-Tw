fbaf3ce6fb894e098668f976c4d5fd9f1ff10527
BogdanFi <bogdan_star1@yahoo.com>
2018-06-19 14:10:13 +0300

    Update produsAdmin.php

--- produsAdmin.php
+++ produsAdmin.php
@@ -1,3 +1,4 @@
+
 <?php
 	include 'db_connection.php'; 
 	$conn = OpenCon();
@@ -20,36 +21,91 @@
 	</head>
 	<body>
 		<header>
+
 			<div class="container">
+
 				<div id="branding">
+
 					<h1><a href="indexAdmin.php"><span class="highlight">AuctioX</span> Web Page</a></h1>
+
 				</div>
+
 				<nav>
+
 				<ul>
+
 					<li><a href="indexAdmin.php">Home</a></li>
+
 					<li><a href="aboutAdmin.php">About us</a></li>
+
 					<li class="current"><a href="categoriesAdmin.php">Categories</a></li>
+
 					<li><a href="accountAdmin.php">Hello admin <?php echo $_SESSION['utilizator']?></a></li>
+
 					<li><a href="logout.php">
+
 					Logout</a></li>							
+
 				</ul>
+
 				</nav>
+
 			</div>
+
 		</header>
 		
-		
-		
-			<section id="newsletter">
+		<section id="newsletter">
 			<div class="container">
 			<form action="searchAdmin.php" method="POST">
-				<input  type="text" name="search" placeholder="Search">
+				<input  type="text" name="search" placeholder="Search" required>
 				<button type="submit" name="submit-search" class="button_1">Search</button>
 			</form>
 
 				
-			</section>
+		</section>
 		
 		<section id="main">
+		    
+			<div class="filter_div">
+				<div class="left_filter">
+				<form  class="filter" method="post">
+					<h4>Grad de uzura </h4>
+					<input type="radio" name="grad_uzura" value="Folosit" > Folosit 
+					<input type="radio" name="grad_uzura"  value="Nou"> Nou 
+					<input class="submit "type="submit" name="submit_g" value="Filter">
+				</form>
+				</div>
+			<!--	<div class="right_filter">
+				<form  class="filter" method="post">
+					<h4> Price range </h4>
+					<input type="number" min=0 max=9999999 name="user_min_price"> Min Price
+					<input type="number" min=0 max=9999999 name="user_min_price"> Max Price
+					<input class="submit "type="submit" name="submit_price" value="Filter">
+				</form>
+				</div> -->
+			</div>
+			<?php
+			
+				$category_name=$_GET['category_name'];
+				
+				if(isset($_POST['submit_g']))
+				{		
+						$grad_uzura=$_POST['grad_uzura'];
+
+						$sql="select * from products 
+						join categories on products.category_id=categories.category_id 
+						join auctions on products.product_id=auctions.product_id
+						WHERE categories.category_name LIKE '%".$category_name."%'
+						AND products.grad_uzura LIKE '%".$grad_uzura."%'						
+						order by products.product_id asc";
+				}
+				else
+					$sql="select * from products 
+						join categories on products.category_id=categories.category_id 
+						WHERE categories.category_name LIKE '%".$category_name."%'  
+						order by product_id asc";						
+			?>
+			
 			<div class="container">		
 				
 				<?php
@@ -57,7 +113,6 @@
 					
 					$category_name=$_GET['category_name'];
 				
-					$sql="select * from products join categories on products.category_id=categories.category_id where categories.category_name LIKE '%".$category_name."%' order by product_id asc";
 					$result=mysqli_query($conn,$sql); // toate coloanele din tabelul products  pentru produsele din categoria transmisa prin $category_name
 				
 					while($row=mysqli_fetch_array($result)) // pentru fiecare produs
@@ -69,6 +124,12 @@
 						$row_result_sql_auction=mysqli_fetch_array($result_sql_auction);  // detalii despre licitatia produsului 
 						
 						$auction_id = $row_result_sql_auction['auction_id'];
+						
+						$sql2="select bidder_id,MAX(bids.bid_amount) from bids join auctions on auctions.auction_id=bids.auction_id join products on auctions.product_id=products.product_id where auctions.product_id=" .$row['product_id']." group by bids.auction_id"; 
+						$result2=mysqli_query($conn,$sql2); 
+						$row2=mysqli_fetch_array($result2);          // pretul curent este cel mai mare bid din tabelul bids corespunzatorul produsului
+						$currentPrice=$row2['MAX(bids.bid_amount)']; // in cazul in care nu a fost plasat nici un bid current price va fi un pret minim stabilit de vanzator in momentul in care completaza formularul de upload, acesta va fi implicit adaugat in tabelul bids
+						$winnerID=$row2['bidder_id'];		// ID-ul userului cu cel mai mare bid corespunzator produsului (poate fi vanzatorul)
 						
 						
 						 if( $current_date < $row_result_sql_auction['end_date'] ) //  daca licitatia este in desfasurare
@@ -94,11 +155,6 @@
 				<p id="descriere-produs">  <?php echo $row["product_description"]; ?> </p>
 				<p id="current-price">Current price :
 					<?php 
-						$sql2="select bidder_id,MAX(bids.bid_amount) from bids join auctions on auctions.auction_id=bids.auction_id join products on auctions.product_id=products.product_id where auctions.product_id=" .$row['product_id']." group by bids.auction_id"; 
-						$result2=mysqli_query($conn,$sql2); 
-						$row2=mysqli_fetch_array($result2);          // pretul curent este cel mai mare bid din tabelul bids corespunzatorul produsului
-						$currentPrice=$row2['MAX(bids.bid_amount)']; // in cazul in care nu a fost plasat nici un bid current price va fi un pret minim stabilit de vanzator in momentul in care completaza formularul de upload, acesta va fi implicit adaugat in tabelul bids
-						$winnerID=$row2['bidder_id'];		// ID-ul userului cu cel mai mare bid corespunzator produsului (poate fi vanzatorul)
 					
 						echo $currentPrice;
 					?>$ 
@@ -109,13 +165,7 @@
 						echo $minBid;
 					?>$
 				</p>
-				<form id="place-bid" action="placebid.php?placeBidProductId='.$row['product_id'].'" method="post" > <!-- Fiecare buton va avea id-ul prodsului corespunzator -->
-					Your Bid:<br>
-					<input type="number" min=<?php echo $minBid; ?>  placeholder="$$$" name="money" required> <!-- Validarea datelor: va permite doar adaugarea numerelor mai mari decat minBid -->
-					<input type="Submit" class="button_bid" name="submit" value="Place Bid">
-					<input type=hidden id='rowid' value="<?php echo $row['product_id']; ?>" name='row_id'> <!-- folosit pentru a trimite id-ul produsului catre placeBid.php -->
-					<input type=hidden id='minbid' value="<?php echo $minBid; ?>" name='min_bid'>
-				</form>					
+								
             </div>
             <?php
                            }	
@@ -123,12 +173,16 @@
 							{						
 							 $sql_find_transaction = "SELECT * FROM transactions WHERE transactions.auction_id='$auction_id'";
 							 $result_find_transaction = mysqli_query($conn,$sql_find_transaction);
-							if( mysqli_num_rows($result_find_transaction) >0) // s-a gasit in transaction licitatia cu id-ul $auction_id
-							{}
+							if( mysqli_num_rows($result_find_transaction) > 0) // s-a gasit in transaction licitatia cu id-ul $auction_id
+							{
+									
+							}
 							else // nu se afla in transactions
-								{
-									$sql_insert_transaction="INSERT INTO transactions ( seller_id, buyer_id,auction_id,amount_of_money) VALUES ( '$sellerID','$winnerID','$auction_id','$currentPrice')";
-									mysqli_query($conn, $sql_insert_transaction);
+								{	if ( $sellerID != $winnerID ) // nu se face tranzactia daca nu a licitat nimeni pentru produs 
+									{	
+										$sql_insert_transaction="INSERT INTO transactions ( seller_id, buyer_id,auction_id,amount_of_money) VALUES ( '$sellerID','$winnerID','$auction_id','$currentPrice')";
+										mysqli_query($conn, $sql_insert_transaction);
+									}
 									
 								} 
 							}
