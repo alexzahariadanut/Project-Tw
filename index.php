a2ece7d37e7bec1cd46d744567293b5b95e90e77
alexzahariadanut <alexzahariadanut@users.noreply.github.com>
2018-06-19 18:17:37 +0300

    Update index.php

--- index.php
+++ index.php
@@ -21,10 +21,11 @@
 				<nav>
 				<ul>
 					<li class="current"><a href="index.php">Home</a></li>
-					<li><a href="about.html">About us</a></li>
+					<li><a href="about.php">About us</a></li>
 					<li><a href="categories.php">Categories</a></li>
-					<li><a href="login.html">Login</a></li>
-					<li><a href="register.html">Register</a></li>
+					<li><a href="login.php">Login</a></li>
+					<li><a href="register.php">Register</a></li>
+					
 					
 				
 				
